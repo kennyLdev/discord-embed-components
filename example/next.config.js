aab2de7b24dd90d5884cc1c90b1fdd59778285a5
Ryan Bucshon <busheezy@users.noreply.github.com>
2023-12-23 15:59:32 -0600

    TEST

--- example/next.config.js
+++ example/next.config.js
@@ -1,6 +1,9 @@
+const githubPagesBuild = process.env.GITHUB_PAGES_BUILD === "true";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: githubPagesBuild ? "/discord-embed-components" : "",
 };
 
 module.exports = nextConfig;
